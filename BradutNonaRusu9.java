41143396600d370456fb1a4a8554d4bd9225bb76
nonarusu6 <58946041+nonarusu6@users.noreply.github.com>
2019-12-16 17:56:47 +0200

    Add files via upload

---
+++ BradutNonaRusu9.java
@@ -0,0 +1,23 @@
+
+public class BradutNonaRusu9 {
+	public static void main (String[] args) {
+		char ch='$';
+		char nm='*';
+		char bn='^';
+		char kf='#';
+		char gh='!';
+		System.out.format("%20c%n",ch);
+		System.out.format("%19c%c%c%n",nm,bn,nm);
+		System.out.format("%18c%c%c%c%c%n",nm,kf,nm,kf,nm);
+		System.out.format("%17c%c%c%c%c%c%c%n",nm,bn,nm,kf,nm,bn,nm);
+		System.out.format("%16c%c%c%c%c%c%c%c%c%n",nm,bn,kf,bn,nm,bn,kf,bn,nm,bn);
+		System.out.format("%15c%c%c%c%c%c%c%c%c%c%c%n",nm,kf,nm,bn,nm,bn,nm,bn,nm,kf,nm);
+		System.out.format("%14c%c%c%c%c%c%c%c%c%c%c%c%c%n",nm,bn,nm,bn,nm,bn,nm,bn,nm,bn,nm,bn,nm,bn);
+		System.out.format("      %13c%c%c%n",gh,gh,gh);
+		System.out.println("            MERRY CHRISTMAS!");
+		
+	
+
+}
+
+}
